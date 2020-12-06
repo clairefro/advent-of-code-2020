2d0a075ab4fc6850c5a5b59ecee594e3434172ee
Claire Froelich <claire.froelich@gmail.com>
2020-12-05 20:30:52 -0800

    Clean code

--- day02.js
+++ day02.js
@@ -3,7 +3,7 @@
 // Policy (Part 1): Each line gives the password policy and then the password. The password policy indicates the lowest and highest number of times a given letter must appear for the password to be valid. For example, 1-3 a means that the password must contain a at least 1 time and at most 3 times.
 // Policy (Part 2): Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
 
-// Load data and parse data --------
+// Load data -----------------------
 const fs = require("fs");
 
 const raw = fs.readFileSync("./inputs/day02.txt", "utf8", function (err, data) {
